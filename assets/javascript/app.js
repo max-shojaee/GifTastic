3e4b451a7bc5a0c4ed5b4b09764e6207d8f81625
Majid <tomajid@yahoo.com>
2017-10-07 09:12:26 -0700

    First Release

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,4 +51,55 @@ function addCar()
 function getCarImages()
 {
     event.preventDefault();
+    
+    var car = $(this).text;
+
+    // Constructing a URL to search Giphy for the name of the person who said the quote
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        car + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // Performing our AJAX GET request
+    $.ajax({
+          url: queryURL,
+          method: "GET"
+    }).done(function(response) {
+
+        var results = response.data;
+
+        // Looping over every result item
+        for (var i = 0; i < results.length; i++) 
+        {
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r") 
+            {
+                  // Creating a div with the class "item"
+                  var gifDiv = $("<div class='item'>");
+
+                  // Storing the result item's rating
+                  var rating = results[i].rating;
+
+                  // Creating a paragraph tag with the result item's rating
+                  var p = $("<p>").text("Rating: " + rating);
+
+                  // Creating an image tag
+                  var carImage = $("<img>");
+
+                  // Giving the image tag an src attribute of a proprty pulled off the
+                  // result item
+                  carImage.attr("src", results[i].images.fixed_height.url);
+
+                  // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                   console.log(p);
+
+                    console.log(carImage);
+
+
+                  gifDiv.append(p);
+                  gifDiv.append(carImage);
+
+                  // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                  $("#carImages").prepend(gifDiv);
+            }
+        }
+    });
 }
